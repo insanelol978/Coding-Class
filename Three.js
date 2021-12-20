6902f248b9414ff1df1e79f8cee360176c1391a4
insanelol978 <joshuabarrus2006@gmail.com>
2021-12-20 14:57:52 -0500

    js

---
+++ Three.js
@@ -0,0 +1,10 @@
+// Goal: Observe the following program and try to predict the values it displays.
+// console.log(4 + 5);
+// console.log("4 + 5");
+// console.log("4" + "5");
+
+// I predict the first and second will output nine. The third will output 35
+
+console.log(4 + 5);
+console.log("4 + 5");
+console.log("4" + "5");
