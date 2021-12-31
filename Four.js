0db373460102c5a5192009e36f77d3a4e1595e30
insanelol978 <joshuabarrus2006@gmail.com>
2021-12-31 13:37:29 -0500

    js

---
+++ Four.js
@@ -0,0 +1,13 @@
+// Goal: Write a program that asks the user for his first name and his last name. The program then displays
+// them in one sentence.
+
+let cl = console.log;
+
+// const name = prompt("Enter your first and last name:");
+// alert(`Hello, $(name)`);
+
+// const name = prompt("Enter your first name:");
+// alert(`Hello, ${name}`);
+
+let name = ("Joshua Barrus");
+cl("Hello " + name);
