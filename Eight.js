c538e3f2934a7a0edea51446abba3208bbb84d83
insanelol978 <joshuabarrus2006@gmail.com>
2022-01-01 20:02:31 -0500

    js

---
+++ Eight.js
@@ -0,0 +1,31 @@
+// Goal: Write a program that accepts a day name from the user, then shows the name of the following
+// day. Incorrect inputs must be taken into account. 
+
+let day = "Tuesday"
+
+if (day = "Sunday"); {
+    console.log("Monday")
+} 
+
+if (day = "Monday") {
+    console.log("Tuesday");
+}
+
+if (day = "Tuesday"); {
+    console.log("Wednesday");
+}
+
+if (day = "Wednesday"); {
+    console.log("Thursday");
+}
+
+if (day = "Thursday"); {
+    console.log("Friday");
+}
+if (day = "Friday"); {
+    console.log("Saturday");
+}
+
+if (day = "Saturday"); {
+    console.log("Sunday");
+}
