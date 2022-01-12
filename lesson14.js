aef876b88c99912c5deb63ac86c939be4d4796d6
insanelol978 <joshuabarrus2006@gmail.com>
2022-01-12 15:37:13 -0500

    js

---
+++ lesson14.js
@@ -0,0 +1,11 @@
+let cl = console.log; 
+
+let d = new Date(); 
+cl(d);
+
+cl(Math.round(Math.random() * 1000));
+
+let array = [1, 2, 3];
+let r = Math.floor(Math.random() * array.length);
+cl(array[r]);
+
