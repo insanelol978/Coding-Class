1e2aa7425f6da50df941a89f75ede62acb36da19
insanelol978 <joshuabarrus2006@gmail.com>
2022-01-13 10:43:55 -0500

    js

---
+++ rps.js
@@ -0,0 +1,42 @@
+let cl = console.log
+
+let getrps = function() {
+    let rps = ["rock", "paper", "scissors"];
+    return rps[Math.floor(Math.random() * rps.length)];
+};
+
+let user = function() {
+    let user = process.argv[2];
+    return user || getrps();
+};
+
+let winner = function(userInput, computerInput) {
+    if(userInput === computerInput) {
+        return "tie";
+    }
+    if (userInput === "rock" && computerInput === "scissors" ||
+    userInput === "scissors" && computerInput === "paper" ||
+    userInput === "paper" && computerInput === "rock"
+    ) {
+    return "Win";
+    } else {
+        return "Lose";
+    }
+};
+
+let congratulate = function(result) {
+    if (result === "tie") {
+        cl("Nobody's a winner.");
+    } else if (result === "Win") {
+        cl("You win!");
+    } else {
+        cl("Sorry, you lost becuase your bad.");
+    }
+};
+
+let userInput = user();
+let computerInput = getrps();
+cl("You picked ", userInput);
+cl("Computer picked", computerInput);
+let result = winner(userInput, computerInput);
+congratulate(result);
