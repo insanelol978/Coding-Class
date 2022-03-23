dc937741238aa3788bfbc36ae769a9148fe5c769
insanelol978 <joshuabarrus2006@gmail.com>
2022-03-23 15:44:46 -0400

    js

---
+++ p5-1.js
@@ -0,0 +1,22 @@
+///<reference path="lib/p5.global-mode.d.ts" />
+
+var setup = function () {
+  createCanvas(600, 400);
+};
+
+var draw = function () {
+  background(100);
+
+  fill(255);
+  circle(175, 125, 50);
+
+  fill(255);
+  circle(375, 125, 50);
+
+  //   arc(175, 250, 375, 250, 250, 250, 175, 255);
+};
+// random
+
+// ideas:
+// disco floor
+// smiley face
